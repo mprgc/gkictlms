a8e9096f466e0cb4d39d0d4a601395bb060d0a30
Gayan Karunathilaka <121455967+mprgc@users.noreply.github.com>
2026-01-05 16:08:22 +0530

    Add files via upload

---
+++ Grade/G10/Q/Questions.js
@@ -0,0 +1,49 @@
+// Questions.js
+
+let originalQuizData = [
+    { 
+        question: "URL යන්නෙහි දිගු ස්වරූපය කුමක්ද?", 
+        correct: "Uniform Resource Locator.", 
+        distractors: [
+            "Universal Record Linkage.", 
+            "Uniform Redirect Language.", 
+            "United Resource Link."
+        ] 
+    },
+    { 
+        question: "වීඩියෝ සම්මන්ත්‍රණ (video conferencing) සඳහා භාවිතා කරන මෘදුකාංග සඳහා උදාහරණයක් දෙන්න.", 
+        correct: "Zoom / Skype.", 
+        distractors: [
+            "Microsoft Word.", 
+            "Adobe Photoshop.", 
+            "Windows Media Player."
+        ] 
+    },
+    { 
+        question: "විද්‍යුත් තැපෑලක ලැබුණු පණිවිඩයක් වෙනත් අයෙකුට යැවීම හැඳින්වෙන්නේ කුමන නමකින්ද?", 
+        correct: "Forward කිරීම.", 
+        distractors: [
+            "Reply කිරීම.", 
+            "Cc කිරීම.", 
+            "Trash කිරීම."
+        ] 
+    },
+    { 
+        question: "විද්‍යුත් තැපෑලක් සමඟ ගොනු (files) යැවීමේ ක්‍රියාවලිය කුමක්ද?", 
+        correct: "Attach කිරීම.", 
+        distractors: [
+            "Download කිරීම.", 
+            "Upload කිරීම.", 
+            "Formatting කිරීම."
+        ] 
+    },
+    { 
+        question: "විද්‍යුත් තැපෑලක 'To' ක්ෂේත්‍රයේ යොදනු ලබන්නේ කුමක්ද?", 
+        correct: "පණිවිඩයේ ප්‍රධාන ලබන්නාගේ ලිපිනය.", 
+        distractors: [
+            "පණිවිඩයේ සාරාංශය.", 
+            "ප්‍රදර්ශන නාමය.", 
+            "අමුණන ලද ගොනුවේ නම."
+        ] 
+    }
+];
